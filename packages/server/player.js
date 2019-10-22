f7e0c14e469f54de5926f4c76ec01f73f29d48a3
Holayn <kwwong1597@gmail.com>
2019-10-22 19:26:22 -0400

    change track works

--- packages/server/player.js
+++ packages/server/player.js
@@ -6,57 +6,44 @@ const decoder = require('lame').Decoder;
 
 module.exports = class Player {
   constructor() {
-    this._createSpeaker();
     this.isPlaying = false;
     this.decoded_stream = null;
     this.audio_stream = null;
   }
 
   play(url) {
+    this._createSpeaker();
+
     if (this.isPlaying) {
       this.changeTrack(url);
+      return;
     }
 
     const audio = ytdl(url, {
       audioFormat: 'mp3',
     });
-  
     const ffmpeg = new FFmpeg(audio);
-  
-    this.isPlaying = true;
-  
-    // ffmpeg.format('mp3').pipe(this.decoded_stream);
-    // this.decoded_stream.pipe(decoder())
-    // .pipe(this.speaker);
 
     let stream = new Stream.PassThrough();
 
-    ffmpeg.format('mp3').pipe(stream);
+    ffmpeg.format('mp3').on('error', (err, stdout, stderr) => {
+      setTimeout(() => {
+        console.log('something went wrong');
+        this.speaker.close();
+        return;
+      }, 3000);
+    }).pipe(stream)
+
     this.decoded_stream = stream.pipe(decoder());
     this.audio_stream = this.decoded_stream.pipe(this.speaker);
 
-    this.audio_stream.on('end', () => {
-      this.decoded_stream.unpipe(this.speaker).end();
-    });
-    this.audio_stream.on('close', () => {
-      this.decoded_stream.unpipe(this.speaker).end();
-    });
+    this.isPlaying = true;
+    
+    console.info('play')
   }
 
   pause() {
     this.decoded_stream.unpipe(this.speaker);
-    this.decoded_stream.on('unpipe', () => {
-      console.log('unpipe');
-    });
-    this.decoded_stream.on('end', () => {
-      console.log('end');
-    });
-    this.decoded_stream.on('close', () => {
-      console.log('close');
-    });
-    this.decoded_stream.on('finish', () => {
-      console.log('finish');
-    });
     this.isPlaying = false;
   }
 
@@ -65,13 +52,25 @@ module.exports = class Player {
     this.isPlaying = true;
   }
 
+  stop() {
+    this.decoded_stream.unpipe(this.speaker).end();
+    this.audio_stream.destroy();
+    this.speaker.close();
+  }
+
   changeTrack(url) {
-    this.stop();
-    this.stream.on('end', () => {
-      console.log('end');
-      this.isPlaying = false;
+    this.isPlaying = false;
+
+    this.audio_stream.on('close', () => {
+      console.info('playing stopped');
       this.play(url);
     });
+
+    this.speaker.on('close',  () => {
+      console.info('closing speaker');
+    });
+
+    this.stop();
   }
 
   _createSpeaker() {
