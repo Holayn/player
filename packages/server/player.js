18716b39558a3ded8ea0a0195a8af81128974762
Holayn <kwwong1597@gmail.com>
2019-10-24 22:16:42 -0400

    refactor into speaker

--- packages/server/player.js
+++ packages/server/player.js
@@ -1,116 +1,22 @@
-const Speaker = require('speaker');
-const ytdl = require('ytdl-core');
-const Stream = require('stream');
-const FFmpeg = require("fluent-ffmpeg");
-const decoder = require('lame').Decoder;
 const ytpl = require('ytpl');
+
+const Speaker = require('./speaker');
 
 module.exports = class Player {
   constructor() {
     this.isPlaying = false;
-    this.decoded_stream = null;
-    this.audio_stream = null;
+    this.speaker = new Speaker();
 
     this.queue = [];
   }
 
-  get playing() {
-    return this.isPlaying;
-  }
-
   load(url) {
-    if (!this.playing && this.queue.length === 0) {
+    if (!this.isPlaying && this.queue.length === 0) {
       this._play(url)
       return;
     }
 
     this.queue.push(url);
-  }
-
-  _play(url) {
-    this._createSpeaker();
-
-    const audio = ytdl(url, {
-      audioFormat: 'mp3',
-    });
-    const ffmpeg = new FFmpeg(audio);
-
-    let stream = new Stream.PassThrough();
-
-    ffmpeg.format('mp3').on('error', (err, stdout, stderr) => {
-      setTimeout(() => {
-        console.log('something went wrong, retrying...');
-        this.speaker.close();
-        this._play(url);
-        return;
-      }, 3000);
-    }).pipe(stream)
-
-    this.decoded_stream = stream.pipe(decoder());
-    this.audio_stream = this.decoded_stream.pipe(this.speaker);
-
-    this.isPlaying = true;
-
-    this.audio_stream.on('close', () => {
-      console.info('finished playing track');
-      this._next();
-    });
-
-    console.info('play')
-  }
-
-  pause() {
-    this.isPlaying = false;
-    this.decoded_stream.unpipe(this.speaker);
-  }
-
-  resume() {
-    this.isPlaying = true;
-    this.decoded_stream.pipe(this.speaker);
-  }
-
-  stop() {
-    this.isPlaying = false;
-    this.decoded_stream.unpipe(this.speaker).end();
-    this.audio_stream.destroy();
-    this.speaker.close();
-  }
-
-  _next() {
-    this._closeAllListeners();
-
-    const next = this.queue.shift();
-    if (next) {
-      console.info('playing next track');
-      this._play(next);
-    }
-  }
-
-  skip() {
-    this._closeAllListeners();
-
-    if (!this.playing) {
-      throw new Error('nothing to skip');
-    }
-
-    let audioStreamClose = new Promise((resolve) => {
-      this.audio_stream.on('close', () => {
-        console.info('playing stopped');
-        resolve();
-      });
-    });
-    let speakerClose = new Promise((resolve) => {
-      this.speaker.on('close', () => {
-        console.info('closing speaker');
-        resolve();
-      });
-    });
-
-    Promise.all([audioStreamClose, speakerClose]).then(() => {
-      this._next();
-    });
-
-    this.stop();
   }
 
   playlist(url) {
@@ -130,21 +36,58 @@ module.exports = class Player {
       if (canPlay) {
         this._next();
       }
-
     });
   }
 
-  _createSpeaker() {
-    this.speaker = new Speaker({
-      channels: 1,
-      bitDepth: 16,
-      sampleRate: 44100,
+  pause() {
+    if (!this.isPlaying) {
+      return;
+    }
+    this.isPlaying = false;
+    this.speaker.pause();
+  }
+
+  resume() {
+    if (this.isPlaying) {
+      return;
+    }
+    this.isPlaying = true;
+    this.speaker.resume();
+  }
+
+  stop() {
+    if (!this.isPlaying) {
+      return;
+    }
+    this.isPlaying = false;
+    this.speaker.stop();
+  }
+
+  skip() {
+    if (!this.isPlaying) {
+      return;
+    }
+
+    this.speaker.stop().then(() => {
+      this._next();
     });
   }
 
-  _closeAllListeners() {
-    if (this.playing) {
-      this.audio_stream.removeAllListeners('close');
+  async _play(url) {
+    this.isPlaying = true;
+
+    await this.speaker.play(url);
+
+    if (this.queue.length > 0) {
+      this._next();
+    }
+  }
+
+  _next() {
+    const next = this.queue.shift();
+    if (next) {
+      console.info('playing next track');
+      this._play(next);
     }
   }
 }
