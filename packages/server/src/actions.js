d5e5b2d65f5686d08548ba8c09125b57467c6600
Holayn <kwwong1597@gmail.com>
2019-11-16 21:29:41 -0500

    get tracks from db and play a random one when alarm is disarmed

--- packages/server/src/actions.js
+++ packages/server/src/actions.js
@@ -1,6 +1,15 @@
-function playMusicHandler(player, subject) {
-  if (subject.match(/play some music/g)) {
-    player.load('https://www.youtube.com/watch?v=ruZ1F4NYWJg');
+const DB = require('./db');
+
+const db = new DB();
+
+const MAIL_MATCHES = [/Ring Alarm is Disarmed/g];
+
+async function playMusicHandler(player, subject) {
+  if (subject.match(MAIL_MATCHES[0])) {
+    const tracks = await db.getTracks();
+
+    const index = Math.floor(Math.random() * Math.floor(tracks.length-1));
+    player.load(tracks[index]);
   }
 }
 
